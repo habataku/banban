e2cb0d961e6c9b3412999785c590aae58c20ed8e
Villager A <100194237+habataku@users.noreply.github.com>
2022-07-01 22:27:36 +0900

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://uvserver1.herokuapp.com/bare/',
+    bare: 'https://bangdrea.herokuapp.com/bare/',
     encodeUrl: Ultraviolet.codec.plain.encode,
     decodeUrl: Ultraviolet.codec.plain.decode,
     handler: '/uv/uv.handler.js',
